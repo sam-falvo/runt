2b02ce6e6f09497ccd84168614d9abb22955a9bb
Samuel A. Falvo II <sam.falvo@rackspace.com>
2012-12-08 00:49:26 -0800

    Discover executables through descent into batch directory.

--- src/github.com/sam-falvo/tx/driver/driver.go
+++ src/github.com/sam-falvo/tx/driver/driver.go
@@ -6,6 +6,7 @@
 package driver
 
 import "fmt"
+import "io/ioutil"
 import "os"
 
 
@@ -15,9 +16,14 @@ import "os"
 // from the Go standard library and host operating system.
 type statFn func (string) (os.FileInfo, error)
 
+// readDirFn is the prototye for the matching io.ioutil.ReadDir() function.
+type readDirFn func (string) ([]os.FileInfo, error)
+
 // This type represents a single test runner instance.
 type Driver struct {
 	stat	statFn
+	readdir	readDirFn
+	executables []string
 }
 
 // DirectoryExpectedError represents an error condition where a directory name was
@@ -39,7 +45,41 @@ func (my *Driver) UseBatch(path string) error {
 		return DirectoryExpectedError
 	}
 
+	my.executables = make([]string, 0)
+
+	return discoverExecutables(my, path)
+}
+
+func discoverExecutables(d *Driver, dir string) error {
+	fis, err := readdir(d, dir)
+	if err != nil {
+		return err
+	}
+	for _, fi := range fis {
+		qn := fmt.Sprintf("%s/%s", dir, fi.Name())
+		if isExecutable(fi) {
+			d.executables = append(d.executables, qn)
+		}
+		if fi.IsDir() {
+			err = discoverExecutables(d, qn)
+			if err != nil {
+				return err
+			}
+		}
+	}
 	return nil
+}
+
+func isExecutable(fi os.FileInfo) bool {
+	return (fi.Mode() & 0110) != 0
+}
+
+// Executables yields the driver's understanding of its batch of test executables
+// to run.  Each filename appears relative to the batch directory; for example,
+// if three executables A, B, and C appear inside the T batch directory, this
+// function will return T/A, T/B, and T/C.
+func (my *Driver) Executables() []string {
+	return my.executables
 }
 
 // UseStat tells the driver which implementation of the stat system call to use.
@@ -52,11 +92,30 @@ func (my *Driver) UseStat(s statFn) {
 	my.stat = s
 }
 
+// UseReadDir tells the driver which implementation of the readdir function
+// to use.  By default, it resorts to using io.ioutil.ReadDir().  However,
+// this call cannot be intercepted for unit-testing purposes; thus, unit tests
+// rely on UseReadDir() to replace the Go library's implementation with their
+// own.
+//
+// Any call to UseReadDir in production code is an error.
+func (my *Driver) UseReadDir(rd readDirFn) {
+	my.readdir = rd
+}
+
 // stat switches between the driver-specific stat procedure or os.Stat.
 func stat(d *Driver, p string) (os.FileInfo, error) {
 	if d.stat != nil {
 		return d.stat(p);
 	}
 	return os.Stat(p)
+}
+
+// readdir switches between the driver-specific ReadDir procedure or io.ioutil.ReadDir
+func readdir(d *Driver, p string) ([]os.FileInfo, error) {
+	if d.readdir != nil {
+		return d.readdir(p)
+	}
+	return ioutil.ReadDir(p)
 }
 
--- src/github.com/sam-falvo/tx/driver/driver_test.go
+++ src/github.com/sam-falvo/tx/driver/driver_test.go
@@ -6,19 +6,125 @@ import "os"
 import "testing"
 import "time"
 
-type fileExistsButNotADirectory struct {};
-type validDirectory struct { fileExistsButNotADirectory };
+type myFileInfo struct {
+	name string
+	mode os.FileMode
+	isDir bool
+};
 
-func (fs *fileExistsButNotADirectory) Name() string { return "don't care" }
-func (fs *fileExistsButNotADirectory) Size() int64 { return 0 }
-func (fs *fileExistsButNotADirectory) Mode() os.FileMode { return 0644 }
-func (fs *fileExistsButNotADirectory) ModTime() time.Time { return time.Now() }
-func (fs *fileExistsButNotADirectory) IsDir() bool { return false }
-func (fs *fileExistsButNotADirectory) Sys() interface{} { return nil }
+func (f *myFileInfo) Name() string { return f.name }
+func (f *myFileInfo) Mode() os.FileMode { return f.mode }
+func (f *myFileInfo) IsDir() bool { return f.isDir }
+func (f *myFileInfo) Sys() interface{} { return nil }
+func (f *myFileInfo) ModTime() time.Time { return time.Now() }
+func (f *myFileInfo) Size() int64 { return 0 }
 
-func (fs *validDirectory) Name() string { return "don't care" }
-func (fs *validDirectory) Mode() os.FileMode { return 0644 | os.ModeDir }
-func (fs *validDirectory) IsDir() bool { return true }
+var fileExistsButNotADirectory = myFileInfo {
+	name: "don't care",
+	mode: 0644,
+	isDir: false,
+}
+
+var validDirectory = myFileInfo {
+	name: "don't care",
+	mode: 0644|os.ModeDir,
+	isDir: true,
+}
+
+func aDir(fn string) *myFileInfo {
+	return &myFileInfo {
+		name: fn,
+		mode: 0644|os.ModeDir,
+		isDir: true,
+	}
+}
+
+func anExec(fn string) *myFileInfo {
+	return &myFileInfo {
+		name: fn,
+		mode: 0755,
+		isDir: false,
+	}
+}
+
+func aFile(fn string) *myFileInfo {
+	return &myFileInfo {
+		name: fn,
+		mode: 0644,
+		isDir: false,
+	}
+}
+
+func emptyDirectory(_ string) ([]os.FileInfo, error) {
+	return make([]os.FileInfo, 0), nil
+}
+
+func basicReadDir(p string) (fis []os.FileInfo, e error) {
+	switch p {
+	case "blah":
+		fis = []os.FileInfo {
+			aFile("a"),
+			aFile("b"),
+			aDir("c"),
+			aDir("d"),
+			anExec("e"),
+			anExec("f"),
+		}
+	default:
+		fis = []os.FileInfo {}
+	}
+	e = nil
+	return
+}
+
+func deepReadDir(p string) (fis []os.FileInfo, e error) {
+	switch p {
+	case "blah":
+		fis = []os.FileInfo {
+			aFile("a"),
+			aFile("b"),
+			aDir("c"),
+			aDir("d"),
+			anExec("e"),
+			anExec("f"),
+		}
+	case "blah/c":
+		fis = []os.FileInfo {
+			anExec("g"),
+			aFile("gg"),
+		}
+	case "blah/d":
+		fis = []os.FileInfo {
+			anExec("h"),
+			aFile("hh"),
+		}
+	default:
+		fis = []os.FileInfo {}
+	}
+	e = nil
+	return
+}
+
+func isElementOf(haystack []string, needle string) (found bool) {
+	found = false
+	for _, h := range haystack {
+		if h == needle {
+			found = true
+		}
+	}
+	return
+}
+
+type testProc func (d *Driver)
+
+func withSetup(rd readDirFn, fiStat os.FileInfo, eStat error, f testProc) {
+	d := new(Driver)
+	d.UseStat(func (_ string) (fi os.FileInfo, e error) {
+		return fiStat, eStat
+	})
+	d.UseReadDir(rd)
+	f(d)
+}
 
 // AS A: developer
 // I WANT: to type in a shell command "run fooBar" to run all integration tests collectively part of the fooBar suite
@@ -30,38 +136,88 @@ func (fs *validDirectory) IsDir() bool { return true }
 //	NOTE: Remember that tests are shell executables.  "run fooBar.exe" is the same as just typing "fooBar.exe".
 
 func TestDriverShouldVerifyBatchIsADirectory(t *testing.T) {
-	d := new(Driver)
-	d.UseStat(func (_ string) (fi os.FileInfo, e error) {
-		return &fileExistsButNotADirectory{}, nil
+	withSetup(emptyDirectory, &fileExistsButNotADirectory, nil, func(d *Driver) {
+		err := d.UseBatch("alksjdhflakjsdf")
+		if err != DirectoryExpectedError {
+			t.Errorf("Expected indicated path must be a directory.")
+		}
 	})
-
-	err := d.UseBatch("alksjdhflakjsdf")
-	if err != DirectoryExpectedError {
-		t.Errorf("Expected indicated path must be a directory.")
-	}
 }
 
 func TestDriverShouldOnlyUseDirectoriesThatExist(t *testing.T) {
-	d := new(Driver)
-	d.UseStat(func (_ string) (fi os.FileInfo, e error) {
-		return nil, os.ErrNotExist
+	withSetup(emptyDirectory, nil, os.ErrNotExist, func(d *Driver) {
+		err := d.UseBatch("MissingDirectory")
+		if err != os.ErrNotExist {
+			t.Errorf("Expected indicated path must exist.")
+		}
 	})
-
-	err := d.UseBatch("MissingDirectory")
-	if err != os.ErrNotExist {
-		t.Errorf("Expected indicated path must exist.")
-	}
 }
 
 func TestDriverShouldNotYieldErrorIfGivenDirectory(t *testing.T) {
-	d := new(Driver)
-	d.UseStat(func (_ string) (fi os.FileInfo, e error) {
-		return &validDirectory{}, nil
+	withSetup(emptyDirectory, &validDirectory, nil, func(d *Driver) {
+		err := d.UseBatch("akjdhf")
+		if err != nil {
+			t.Errorf("Unexpected error when given a directory: %s", err.Error())
+		}
 	})
+}
 
-	err := d.UseBatch("akjdhf")
-	if err != nil {
-		t.Errorf("Unexpected error when given a directory: %s", err.Error())
-	}
+//	AS A: implementor
+//	I WANT: runt to iterate through all the directory entries in fooBar, recursively
+//	SO THAT: we can decide which are executable files and which are not.
+
+func TestDriverMustIsolateExecutablesInBatchDir(t *testing.T) {
+	withSetup(basicReadDir, aDir("blah"), nil, func(d *Driver) {
+		_ = d.UseBatch("blah")
+		if len(d.Executables()) != 2 {
+			t.Errorf("Gave a simple directory with 2 executables, but discovered %d", len(d.Executables()))
+		}
+	})
+}
+
+func TestDriverMustRecurseIntoSubdirectories(t *testing.T) {
+	pathsRead := make([]string, 0, 4)
+	withSetup(
+		func (path string) (fis []os.FileInfo, e error) {
+			pathsRead = append(pathsRead, path)
+			return basicReadDir(path)
+		},
+		aDir("blah"),
+		nil,
+		func (d *Driver) {
+			_ = d.UseBatch("blah")
+			if len(pathsRead) != 3 {
+				t.Errorf("Expected to traverse 3 directories; only %d traversed (%#v).", len(pathsRead), pathsRead)
+			}
+			if !isElementOf(pathsRead, "blah") {
+				t.Errorf("Expected to traverse root of batch directory")
+			}
+			if !isElementOf(pathsRead, "blah/c") {
+				t.Errorf("Expected to traverse blah/c")
+			}
+			if !isElementOf(pathsRead, "blah/d") {
+				t.Errorf("Expected to traverse blah/d")
+			}
+		},
+	)
+}
+
+func TestDriverMustQualifyExecutablePathNames(t *testing.T) {
+	withSetup(deepReadDir, aDir("blah"), nil, func (d *Driver) {
+		_ = d.UseBatch("blah")
+		es := d.Executables()
+		if !isElementOf(es, "blah/e") {
+			t.Errorf("Expected to discover executable blah/e")
+		}
+		if !isElementOf(es, "blah/f") {
+			t.Error("Expected to discover executable blah/f")
+		}
+		if !isElementOf(es, "blah/c/g") {
+			t.Error("Expected to discover executable blah/c/g")
+		}
+		if !isElementOf(es, "blah/d/h") {
+			t.Error("Expected to discover executable blah/d/h")
+		}
+	})
 }
 
